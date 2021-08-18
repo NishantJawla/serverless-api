aead740af39d4516b5fe2bdd99fefc911803c835
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-08-19 00:31:15 +0530

    Added Redirect by seeing Roles

--- src/index.js
+++ src/index.js
@@ -35,12 +35,18 @@ app.post('/readuser', (req, res) => {
     
     docClient.get(params, function (err, data) {
         if (err) {
-            console.log("Error in Fetching User - " + JSON.stringify(err, null, 2));
+            console.log("Error in Fetching User : " + JSON.stringify(err, null, 2));
+            res.status(500).json({
+                error: "Server Failure!!"
+            })
         }
         else {
-            console.log("Succesfully Fetching the User - " + JSON.stringify(data, null, 2));
+            console.log("Succesfully Fetching the User : " + JSON.stringify(data, null, 2));
             if(Object.keys(data).length === 0){
                 console.log("User Not Found!!!")
+                res.status(404).json({
+                    error: "Invalid email/ phoneNumber"
+                })
             } else {
                 const response = data.Item;
                 console.log(response)
@@ -53,8 +59,18 @@ app.post('/readuser', (req, res) => {
                 docClient.get(paramsforusertable, function (err, data) {
                     if(err) {
                         console.log("unable to retrieve data from server uuid serverless table")
+                        res.status(500).json({
+                            error: "Server Failure!!"
+                        })
                     } else {
-                        res.status(200).json(data)
+                        const role = data.Item.role;
+                        if(role === "superadmin"){
+                            res.redirect('https://www.google.com/')
+                        } else if(role === "companyadmin"){
+                            res.redirect('https://www.youtube.com/')
+                        } else {
+                            res.redirect('https://github.com/')
+                        }
                     }
                 })
             }
