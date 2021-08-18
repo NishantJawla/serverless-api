1b57d3e42bfe54696affb5afef855833be50ae2e
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-08-19 00:40:06 +0530

    Created the login route

--- src/index.js
+++ src/index.js
@@ -79,4 +79,76 @@ app.post('/readuser', (req, res) => {
     })
 })
 
+
+app.post('/signin', (req, res) => {
+    const value = req.body.value;
+    var dataType = "phonenumber"
+    if(value.includes("@")){
+        console.log("Value is an Email")
+        dataType = "email";
+    } 
+    
+    if(dataType === 'email'){
+        var params = {
+            TableName: "emailserverless",
+            Key: {
+                "useremail": value
+            }
+        };
+    } else {
+        var params = {
+            TableName: "phonenumberserverless",
+            Key: {
+                "userphonenumber": value
+            }
+        };
+    }
+    
+    docClient.get(params, function (err, data) {
+        if (err) {
+            console.log("Error in Fetching User : " + JSON.stringify(err, null, 2));
+            res.status(500).json({
+                error: "Server Failure!!"
+            })
+        }
+        else {
+            console.log("Succesfully Fetching the User : " + JSON.stringify(data, null, 2));
+            if(Object.keys(data).length === 0){
+                console.log("User Not Found!!!")
+                res.status(404).json({
+                    error: "Invalid email/ phoneNumber"
+                })
+            } else {
+                const response = data.Item;
+                console.log(response)
+                const paramsforusertable = {
+                    TableName: "uuidserverless",
+                    Key: {
+                        "uuid": response.uuid
+                    }
+                };
+                docClient.get(paramsforusertable, function (err, data) {
+                    if(err) {
+                        console.log("unable to retrieve data from server uuid serverless table")
+                        res.status(500).json({
+                            error: "Server Failure!!"
+                        })
+                    } else {
+                        if(req.body.password === data.Item.password) {
+                            res.status(200).json({
+                                msg: "Congratulations! On login succesfull"
+                            })
+                        } else {
+                            res.status(200).json({
+                                error: "Password is Invalid"
+                            })
+                        }
+                    }
+                })
+            }
+
+        }
+    })
+})
+
 module.exports = app;
